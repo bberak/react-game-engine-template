5e827771bb0dea1a1323925970af6a95f1a205af
bberak <bberak@gmail.com>
2019-09-08 22:31:08 +1000

    Allow spawning using mouse buttons

--- src/game/systems/spawn.js
+++ src/game/systems/spawn.js
@@ -5,15 +5,15 @@ import { id } from "../utils";
 const boxId = (id => () => id("box"))(id(0));
 const cylinderId = (id => () => id("cylinder"))(id(0));
 
-const Spawn = (entities, { gamepadController }) => {
+const Spawn = (entities, { gamepadController, mouseController }) => {
 
   const world = entities.world;
   const scene = entities.scene;
 
-  if (gamepadController.button0 && !gamepadController.previous.button0)
+  if ((gamepadController.button0 && !gamepadController.previous.button0) || (mouseController.left && !mouseController.previous.left))
   	entities[boxId()] = Box({ parent: scene, world, y: 5 });
 
-  if (gamepadController.button1 && !gamepadController.previous.button1)
+  if ((gamepadController.button1 && !gamepadController.previous.button1) || (mouseController.right && !mouseController.previous.right))
   	entities[cylinderId()] = Cylinder({ parent: scene, world, y: 5 });
 
   return entities;
